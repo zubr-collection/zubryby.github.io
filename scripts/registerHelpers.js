c7e76dfa7f5568a467081a1e0ce428de82ae556c
Artur Basak <artur.basak.devingrodno@gmail.com>
2019-10-02 23:02:42 +0300

    updates

--- scripts/registerHelpers.js
+++ scripts/registerHelpers.js
@@ -4,17 +4,17 @@ const Handlebars = require('handlebars');
 const readTemplate = require('./readTemplate');
 
 function registerHelpers() {
-  const { SafeString } = Handlebars;
-  Handlebars.registerHelper('header', () => new SafeString(readTemplate('fragments/header.hbs')({})));
-  Handlebars.registerHelper('footer', () => new SafeString(readTemplate('fragments/footer.hbs')({})));
-  Handlebars.registerHelper(
-    'seo',
-    (title, picture) => new SafeString(readTemplate('fragments/seo.hbs')({ title, picture }))
-  );
-  Handlebars.registerHelper(
-    'resources',
-    addSwiper => new SafeString(readTemplate('fragments/resources.hbs')({ addSwiper }))
-  );
+    const { SafeString } = Handlebars;
+    Handlebars.registerHelper('header', () => new SafeString(readTemplate('fragments/header.hbs')({})));
+    Handlebars.registerHelper('footer', () => new SafeString(readTemplate('fragments/footer.hbs')({})));
+    Handlebars.registerHelper(
+        'seo',
+        (title, picture) => new SafeString(readTemplate('fragments/seo.hbs')({ title, picture }))
+    );
+    Handlebars.registerHelper(
+        'resources',
+        addSwiper => new SafeString(readTemplate('fragments/resources.hbs')({ addSwiper }))
+    );
 }
 
 module.exports = registerHelpers;
