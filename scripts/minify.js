12597d442cf14e81235f34714906ad0b422453b6
Artur Basak <artur.basak.devingrodno@gmail.com>
2019-10-02 17:28:55 +0300

    updates

--- scripts/minify.js
+++ scripts/minify.js
@@ -1,17 +1,26 @@
 const fs = require('fs');
 const path = require('path');
-const uglify = require("uglify-es");
+const uglify = require('uglify-es');
+const cleancss = require('clean-css');
 
 function minify() {
-    const scriptNames = fs.readdirSync(path.join(__dirname, '../assets/js')).filter(name => !name.includes('.min'));
-    for (let i = 0; i < scriptNames.length; i++) {
-        const scriptName = scriptNames[i];
-        const scriptSourceCode = fs.readFileSync(path.join(__dirname, `../assets/js/${scriptName}`)).toString();
-        const result = uglify.minify(scriptSourceCode);
-        if (result.error) {
-            console.error(result.error);
-        }
-        fs.writeFileSync(path.join(__dirname, `../assets/js/${scriptName.replace('.js', '')}.min.js`), result.code);
+  const scriptNames = fs.readdirSync(path.join(__dirname, '../assets/js')).filter(name => !name.includes('.min'));
+  for (let i = 0; i < scriptNames.length; i++) {
+    const scriptName = scriptNames[i];
+    const scriptSourceCode = fs.readFileSync(path.join(__dirname, `../assets/js/${scriptName}`)).toString();
+    const result = uglify.minify(scriptSourceCode);
+    if (result.error) {
+      console.error(result.error);
+    }
+    fs.writeFileSync(path.join(__dirname, `../assets/js/${scriptName.replace('.js', '')}.min.js`), result.code);
+  }
+
+    const styleNames = fs.readdirSync(path.join(__dirname, '../assets/css')).filter(name => !name.includes('.min'));
+    for (let i = 0; i < styleNames.length; i++) {
+        const styleName = styleNames[i];
+        const styleSourceCode = fs.readFileSync(path.join(__dirname, `../assets/css/${styleName}`)).toString();
+        const result = (new cleancss()).minify(styleSourceCode);
+        fs.writeFileSync(path.join(__dirname, `../assets/css/${styleName.replace('.css', '')}.min.css`), result.styles);
     }
 }
 
